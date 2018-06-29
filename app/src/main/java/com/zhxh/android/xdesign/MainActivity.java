b702997e4ff1c4957edfd125d61adbe3bf316833
zhxh <zhxhcoder@gmail.com>
2018-06-29 12:46:57 +0800

    ViewUtil.parseColor

--- app/src/main/java/com/zhxh/android/xdesign/MainActivity.java
+++ app/src/main/java/com/zhxh/android/xdesign/MainActivity.java
@@ -1,7 +1,10 @@
 package com.zhxh.android.xdesign;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.zhxh.xdesignlib.ViewUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv1 = findViewById(R.id.tv1);
+        TextView tv2 = findViewById(R.id.tv2);
+        TextView tv3 = findViewById(R.id.tv3);
+
+        tv1.setTextColor(ViewUtil.parseColor("#0000CD"));
+        tv2.setTextColor(ViewUtil.parseColor("#500000CD"));
+        tv3.setTextColor(ViewUtil.parseColor("#FFD700"));
+
     }
 }
