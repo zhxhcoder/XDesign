356e594db0721214ce6778c430d964ab9925aa5b
zhxh <zhxhcoder@gmail.com>
2018-06-29 11:34:46 +0800

    xdesignlib

--- xdesignlib/src/main/java/com/zhxh/xdesignlib/ViewUtil.java
+++ xdesignlib/src/main/java/com/zhxh/xdesignlib/ViewUtil.java
@@ -2,14 +2,19 @@ package com.zhxh.xdesignlib;
 
 import android.graphics.Color;
 
+import com.zhxh.xtextlib.XString;
+
 /**
  * Created by zhxh on 2018/6/29
  */
 public class ViewUtil {
 
     public static int parseColor(String colorString) {
-        return Color.parseColor("#bc29e3");
-    }
 
+        if (XString.isColorStr(colorString)) {
+            return Color.parseColor(colorString);
+        }
+        return 0;
+    }
 
 }
