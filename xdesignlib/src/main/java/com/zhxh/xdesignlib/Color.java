ad3dcfad3ef747ee7627b823b9d50b640bec85a4
zhxh <zhxhcoder@gmail.com>
2018-06-29 10:45:53 +0800

    xdesignlib

---
+++ xdesignlib/src/main/java/com/zhxh/xdesignlib/Color.java
@@ -0,0 +1,13 @@
+package com.zhxh.xdesignlib;
+
+/**
+ * Created by zhxh on 2018/6/29
+ */
+public class Color {
+
+    public static int parseColor(String colorString) {
+        return Color.parseColor("#bc29e3");
+    }
+
+
+}
