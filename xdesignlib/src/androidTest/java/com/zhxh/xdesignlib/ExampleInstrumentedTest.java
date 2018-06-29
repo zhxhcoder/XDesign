fe292f57edb214fc0850efc3c2b258165766caf0
zhxh <zhxhcoder@gmail.com>
2018-06-29 10:42:44 +0800

    xdesignlib

--- xdesignlib/src/androidTest/java/com/zhxh/xdesignlib/ExampleInstrumentedTest.java
+++ xdesignlib/src/androidTest/java/com/zhxh/xdesignlib/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.zhxh.android.xdesign;
+package com.zhxh.xdesignlib;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.zhxh.android.xdesign", appContext.getPackageName());
+        assertEquals("com.zhxh.xdesignlib.test", appContext.getPackageName());
     }
 }
--- xdesignlib/src/test/java/com/zhxh/xdesignlib/ExampleUnitTest.java
+++ xdesignlib/src/test/java/com/zhxh/xdesignlib/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.zhxh.android.xdesign;
+package com.zhxh.xdesignlib;
 
 import org.junit.Test;
 
