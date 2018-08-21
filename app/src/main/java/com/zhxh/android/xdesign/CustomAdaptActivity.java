11e2aba8ac00229df4f2ad9accd4b6c7ea5070b7
zhxh <zhxhcoder@gmail.com>
2018-08-21 14:28:29 +0800

    design

---
+++ app/src/main/java/com/zhxh/android/xdesign/CustomAdaptActivity.java
@@ -0,0 +1,56 @@
+package com.zhxh.android.xdesign;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhxh.autosize.internal.CustomAdapt;
+
+/**
+ * ================================================
+ * 本框架核心原理来自于 <a href="https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA">今日头条官方适配方案</a>
+ * <p>
+ * {@link CustomAdaptActivity} 展示项目内部的 {@link Activity} 自定义适配参数的用法, 需要实现 {@link CustomAdapt}
+ * 现在 AndroidAutoSize 是全局以屏幕宽度为基准进行适配的, 并且全局的设计图尺寸为 360 * 640
+ * 这里就展示怎么让 {@link CustomAdaptActivity} 单个页面, 有别于全局设置, 以屏幕高度为基准进行适配, 并且更改设计图尺寸为 IPhone 的设计图尺寸
+ * 如果这个页面的设计图尺寸有别于其他页面, AndroidAutoSize 允许您改变单个页面的设计图尺寸, {@link #getSizeInDp()}
+ * <p>
+ * Created by JessYan on 2018/8/11 11:31
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class CustomAdaptActivity extends AppCompatActivity implements CustomAdapt {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_adapt);
+    }
+
+    /**
+     * 是否按照宽度进行等比例适配 (为了保证在高宽比不同的屏幕上也能正常适配, 所以只能在宽度和高度之中选一个作为基准进行适配)
+     *
+     * @return {@code true} 为按照宽度适配, {@code false} 为按照高度适配
+     */
+    @Override
+    public boolean isBaseOnWidth() {
+        return false;
+    }
+
+    /**
+     * 这里使用 IPhone 的设计图, IPhone 的设计图尺寸为 750px * 1334px, 高换算成 dp 为 667 (1334px / 2 = 667dp)
+     * <p>
+     * 返回设计图上的设计尺寸, 单位 dp
+     * {@link #getSizeInDp} 须配合 {@link #isBaseOnWidth()} 使用, 规则如下:
+     * 如果 {@link #isBaseOnWidth()} 返回 {@code true}, {@link #getSizeInDp} 则应该返回设计图的总宽度
+     * 如果 {@link #isBaseOnWidth()} 返回 {@code false}, {@link #getSizeInDp} 则应该返回设计图的总高度
+     * 如果您不需要自定义设计图上的设计尺寸, 想继续使用在 AndroidManifest 中填写的设计图尺寸, {@link #getSizeInDp} 则返回 {@code 0}
+     *
+     * @return 设计图上的设计尺寸, 单位 dp
+     */
+    @Override
+    public float getSizeInDp() {
+        return 667;
+    }
+}
---
+++ xdesignlib/src/main/java/com/zhxh/xdesignlib/DefaultErrorActivity.java
@@ -0,0 +1,13 @@
+package com.zhxh.xdesignlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DefaultErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default_error);
+    }
+}
